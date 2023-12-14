1ca33ae2e08a082f3d98c9b9c9a728319ff885e6
vygantaspaul <wygantas.pauliukonis@gmail.com>
2023-12-14 18:11:47 +0200

    update

--- src/routes/locations.js
+++ src/routes/locations.js
@@ -6,7 +6,7 @@ router.get('/locations/random', auth, GET_LOCATION_RANDOM)
 router.post('/locations', auth, ADD_LOCATION)
 router.get('/locations', auth, GET_USERS_LOCATIONS)
 router.get('/locations/:id', auth, GET_LOCATION_BY_ID)
-router.post('/locations/:id', auth, PUT_LOCATION)
+router.put('/locations/:id', auth, PUT_LOCATION)
 router.delete('/locations/:id', auth, DELETE_LOCATION)
 
 export { GET_LOCATIONS, ADD_LOCATION, DELETE_LOCATION, GET_LOCATION_BY_ID, PUT_LOCATION, GET_LOCATION_RANDOM, GET_USERS_LOCATIONS }
